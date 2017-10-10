abb6a466d88003cd380086af688a227bbcd46a18
Dmitriy.P <petrukhindmitriy@gmail.com>
2017-10-10 16:10:28 +0300

    first commit

---
+++ src/test/app.java
@@ -0,0 +1,13 @@
+package test;
+
+public class app {
+    public static void main(String[] args) {
+        int age = 40;
+        if (age > 30) {
+            System.out.println("Старпер!");
+        } else {
+            System.out.println(+age + " лет");
+        }
+
+    }
+}
